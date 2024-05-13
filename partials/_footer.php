2b89bdbba14ec8d0a088530d24d3641a875f5d62
MSoffar <159565280+MSoffar@users.noreply.github.com>
2024-05-13 15:37:12 +0300

    Add files via upload

---
+++ partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0"><a href="https://github.com/MSoffar" target="_blank" rel="noopener noreferrer">Hamza Hesham Hendy 7768 // Mohamed Khaled Soffar 7686 // Omar Elsharkawy 7446</a></p>
+</div>
